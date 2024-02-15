4c272284d6b157aab987a58548428f5e3297cd85
Nilesh <nilesh.modi.nitb@gmail.com>
2024-02-16 01:12:28 +0530

    Handling Exceptions and customizing error messages

--- src/main/java/com/springboot/blog/SpringbootBlogAppApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogAppApplication.java
@@ -1,10 +1,17 @@
 package com.springboot.blog;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootBlogAppApplication {
+	
+	@Bean
+	public ModelMapper modelmapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogAppApplication.class, args);
